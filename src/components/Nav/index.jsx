57e6c3b05d3b663cc4c26cfbf4a5009eddd3f3d0
Sarah-Hashmi-Codes <114784014+Sarah-Hashmi-Codes@users.noreply.github.com>
2023-02-25 10:48:33 +1100

    add css

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import variable from pathfolder
+import Logo from "../../assets/logo-hi-res-trans.png"
+import "./nav.css"
 
 function Nav() {
     return (
 
         <nav className="row block center">
-            {/* <img src={variable} /> */}
+            <Link to="/">
+                <img src={Logo} height={50} width={80} alt="logo"></img>
+            </Link>
             
             <Link className="" to="/">Home</Link>
             <Link to='/createproject'>Create Campaign</Link>
