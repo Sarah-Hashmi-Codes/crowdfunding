619d49755540971ff6bb13dec9dca933ef662649
Sarah-Hashmi-Codes <114784014+Sarah-Hashmi-Codes@users.noreply.github.com>
2023-04-13 14:33:31 +1000

    Update index.jsx

--- src/components/PledgeForm.jsx/index.jsx
+++ src/components/PledgeForm.jsx/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 function PledgeForm() {
+    const { id:projectId } = useParams();
+
     const [ formData, setFormData ] = useState({
         amount: "",
         comment: "",
@@ -16,7 +18,8 @@ function PledgeForm() {
         const { id, value } = event.target;    //deconstruction
         setFormData((prevFormData) => ({
             ...prevFormData,
-            [id] : value
+            [id] : value,
+            project: projectId
         }));
     
     };
@@ -24,25 +27,35 @@ function PledgeForm() {
     const navigate = useNavigate();
     
    
-    const handleSubmit = (event) => {
-        event.preventDefault(); 
+    // const handleSubmit = (event) => {
+    //     event.preventDefault(); 
     
     
-       {postData().then((response) => {
-    console.log(response)
+    //    {postData().then((response) => {
+    // console.log(response)
 
-    navigate(`/project/${response.id}`);
+    // navigate(`/project/${response.id}`);
+    //         })
+    //     };
+    
+    // };
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); //Dont send anything get, want to add logic
+        {postData().then((response) => {
+            // console.log(response)
+            alert("Your Donation was successful")
+            navigate(`/project/${projectId}`);
             })
         };
-    
-    };
+    }
 
 
 
     const postData = async ()  => {
         const token = window.localStorage.getItem('token')
         const response = await 
-        fetch(`${import.meta.env.VITE_API_URL}pledges`, {
+        fetch(`${import.meta.env.VITE_API_URL}pledges/`, {
             method: 'post',
             headers: {                                 
                 'Content-Type': "application/json",  
@@ -67,7 +80,7 @@ function PledgeForm() {
 
         <div>
             <label htmlFor='comment'>Comment</label>
-            <input onChange={handleChange} type="text" id='title' placeholder="Your comment"></input>    
+            <input onChange={handleChange} type="text" id='comment' placeholder="Your comment"></input>    
         </div>
 {/* 
         <div>
